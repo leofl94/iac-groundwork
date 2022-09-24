178ffbc6955de763f99f2d624f4b66fff3c5408c
Leonardo Ferreira de Lima <leofl94@gmail.com>
2022-09-23 23:07:06 -0300

    Alterando nomenclaturas

--- main.tf
+++ main.tf
@@ -1,24 +1,28 @@
+terraform {
+  backend "gcs" {
+    bucket  = "mentoria-tfstate-staging"
+    prefix  = "groundwork/state"
+  }
+}
+
 provider "google" {
-  project = "mentoriaiac-20220916"
+  project = "mentoria-iac-staging"
   region  = "us-central1"
 }
 
-resource "google_compute_network" "vpc" {
-  name                    = "vpc-network"
+resource "google_compute_network" "orquestradores" {
+  name                    = "orquestradores"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "load-balance" {
-  name          = "load-balance"
+resource "google_compute_subnetwork" "load-balancer" {
+  name          = "load-balancer"
   ip_cidr_range = "10.2.0.0/16"
-  network       = google_compute_network.vpc.id
-
+  network       = google_compute_network.orquestradores.id
 }
 
 resource "google_compute_subnetwork" "nomad" {
   name          = "nomad"
   ip_cidr_range = "10.1.0.0/16"
-  network       = google_compute_network.vpc.id
-
+  network       = google_compute_network.orquestradores.id
 }
-
